0ae769fa46a42e870500b95895b0fc14ac4255dd
Mourad KIRAT <mourad.kirat.9@gmail.com>
2025-01-09 17:02:31 +0100

    Initial Commit

---
+++ AppLocalAuthenticationBiometric/AppLocalAuthenticationBiometricApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppLocalAuthenticationBiometricApp.swift
+//  AppLocalAuthenticationBiometric
+//
+//  Created by Mourad KIRAT on 9/1/2025.
+//
+
+import SwiftUI
+
+@main
+struct AppLocalAuthenticationBiometricApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
